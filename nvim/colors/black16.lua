85fd1f9fa29dd31794df03f31fe22c28c1b72afc
tomsadowski <tsadowski93@gmail.com>
2025-07-02 06:33:59 -0400

    such color

--- nvim/colors/black16.lua
+++ nvim/colors/black16.lua
@@ -3,18 +3,25 @@
 local p1 = {
     A1 = '#000000', -- black
     A2 = '#000000', -- black
-    A3 = '#808080', -- grey
-    A4 = '#00ffff', -- aqua
+
     B1 = '#ffffff', -- white
-    B2 = '#00ff00', -- lime
-    B3 = '#808080', -- grey
-    B4 = '#ffff00', -- yellow
-    B6 = '#ffff00', -- yellow
-    B5 = '#ffff00', -- yellow
-    C1 = '#00ff00', -- lime
-    C2 = '#ff00ff', -- fuschia
+
     C3 = '#808080', -- grey
-    C4 = '#ffff00', -- yellow
+    A3 = '#808080', -- grey
+    B3 = '#808080', -- grey
+
+    A4 = '#ff00ff', -- fuschia
+    C1 = '#ff00ff', -- fuschia
+
+    B2 = '#00ffff', -- cyan
+    C4 = '#00ffff', -- cyan
+
+    B6 = '#00ff00', -- green
+    
+    B5 = '#ff0000', -- red
+    B4 = '#ff0000', -- red
+
+    C2 = '#ffff00', -- yellow
 }
 
 require("color").schemes.A.apply(p1)
--- nvim/colors/blue.lua
+++ nvim/colors/blue.lua
@@ -3,20 +3,25 @@
 local p1 = {
     A1 = '#000000', -- black
     A2 = '#000408', -- black
-    A3 = '#6084a8', -- grey
-    A4 = '#9988dd', -- purple
-    B1 = '#c0eeff', -- white
-    B2 = '#44aadd', -- mild cyan
-    B3 = '#6084a8', -- grey
 
-    B6 = '#66aa88', -- green
-    B5 = '#aa77aa', -- muted pink
-    B4 = '#aa77aa', -- muted pink
-    C2 = '#ccbb60', -- fire
+    B1 = '#c0eeff', -- white
+
+    A3 = '#6084a8', -- grey
+    B3 = '#6084a8', -- grey
+    C3 = '#6084a8', -- grey
 
     C1 = '#9988dd', -- purple
-    C3 = '#6084a8', -- grey
+    A4 = '#9988dd', -- purple
+
+    B2 = '#44aadd', -- mild cyan
     C4 = '#44aadd', -- mild cyan
+
+    B6 = '#66aa88', -- green
+
+    B5 = '#aa77aa', -- muted pink
+    B4 = '#aa77aa', -- muted pink
+
+    C2 = '#ccbb60', -- fire
 }
 
 require("color").schemes.A.apply(p1)
