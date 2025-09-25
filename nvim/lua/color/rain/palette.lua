08873752592eb753d3d4a17a384cecc6af1c048b
tomsadowski <tsadowski93@gmail.com>
2025-09-24 20:19:37 -0400

    rain yes

--- nvim/lua/color/rain/palette.lua
+++ nvim/lua/color/rain/palette.lua
@@ -5,21 +5,16 @@ local p = vim.deepcopy(require("color.common.palette"))
 p.darkcyan[1] = "#0c1420" -- _normal
 p.cyan[3]     = "#103040" -- _linenumber
 p.cyan[1]     = "#206060" -- _comment
-p.green[2]    = "#20a060" -- _var
-p.green[1]    = "#30e080" -- _mut_var
-
 p.cyan[2]     = "#587890" -- _keyword
 p.yellow[2]   = "#a88078" -- _function
-p.yellow[1]   = "#a8a878" -- _normal
---p.yellow[1]   = "#e8b860" -- _normal
-p.red[1]      = "#ff6060" -- _enum, _typeparam
+p.yellow[1]   = "#a8a860" -- _normal
+p.grey[1]     = "#907860" -- _string
 
-
-
-
-p.grey[1]     = "#b08860" -- _string
-p.magenta[1]  = "#ff80ff" -- _mut_funcparam
-p.magenta[2]  = "#b090d0" -- _funcparam
+p.green[1]    = "#20e00c" -- _mut_var
+p.green[2]    = "#0ca020" -- _var
+p.magenta[1]  = "#e020e0" -- _mut_funcparam
+p.magenta[2]  = "#a040a0" -- _funcparam
+p.red[1]      = "#e04040" -- _enum, _typeparam
 
 
 
