c0199f30a97b9705b06b702981e6f9de2165c950
tomsadowski <tsadowski93@gmail.com>
2025-10-08 22:11:32 -0400

    day2

---
+++ nvim/colors/day2.lua
@@ -0,0 +1,7 @@
+-- day
+
+local f = require "color.util"
+local p = require "color.palette_day2"
+local s = require "color.scheme_day"
+
+f.apply_spec({palette = p, scheme = s})
--- nvim/init.lua
+++ nvim/init.lua
@@ -3,5 +3,6 @@ require "keymap"
 require "lsp"
 --require "treesitter"
 
-vim.cmd "colo blackday"
+vim.cmd "colo day2"
 
+vim.cmd("com LspCap lua =vim.lsp.get_clients()[1].server_capabilities")
