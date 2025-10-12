183d147730e3f243e88f253c202006dac847c55b
tomsadowski <tsadowski93@gmail.com>
2025-10-12 14:14:06 -0400

    lars is best

--- nvim/lua/color/scheme_simple.lua
+++ nvim/lua/color/scheme_simple.lua
@@ -44,31 +44,17 @@ return function(p) return {
         ctermfg = t.magenta,
         fg      = p.magenta[1],
     }, 
-    _mut_var       = {link = "_var"}, 
-    _funcparam     = {link = "_var"}, 
-    _mut_funcparam = {link = "_var"}, 
-    _macro         = {link = "_normal"}, 
-    _doc_comment   = {link = "_comment"}, 
-    _enummember    = {link = "_enum"},
-    _typeparam     = {link = "_enum"},
-    _interface     = {link = "_function"}, 
-    _constant      = {link = "_var"}, 
-
     _linenumber = {
-        ctermfg = t.darkblue,
-        fg      = p.darkblue[1],
+        ctermfg = t.darkgrey,
+        fg      = p.darkgrey[1],
     }, 
-    _cursorlinenumber = {
-        ctermfg = t.darkcyan,
-        fg      = p.darkcyan[1],
-    }, 
+    _visual     = {
+        ctermbg = t.darkblue,
+        bg      = p.darkblue[1],
+    },
     _matchparen = {
         ctermbg = t.darkyellow,
         bg      = p.darkyellow[2],
-    }, 
-    _visual = {
-        ctermbg = t.darkblue,
-        bg      = p.darkblue[1],
     }, 
     _search = {
         ctermfg = t.blue,
@@ -88,33 +74,14 @@ return function(p) return {
         ctermbg = t.darkblue,
         bg      = p.darkblue[2],
     }, 
-    _bold = {
-        bold    = true,
-        ctermfg = t.yellow,
-        fg      = p.yellow[1],
-    }, 
-    _bad = {
-        ctermfg = t.red,
-        fg      = p.red[1],
-    }, 
-    _warn = {
-        ctermfg = t.darkmagenta,
-        fg      = p.darkmagenta[1],
-    }, 
-    _hide = {
-        ctermfg = t.cyan,
-        fg      = p.cyan[1],
-    }, 
-    _added = {
-        ctermfg = t.green,
-        fg      = p.green[1],
-    }, 
-    _changed = {
-        ctermfg = t.darkmagenta,
-        fg      = p.darkmagenta[1],
-    }, 
-    _removed = {
-        ctermfg = t.red,
-        fg      = p.red[1],
-    }, 
+    _mut_var          = {link = "_var"}, 
+    _funcparam        = {link = "_var"}, 
+    _mut_funcparam    = {link = "_var"}, 
+    _macro            = {link = "_normal"}, 
+    _doc_comment      = {link = "_comment"}, 
+    _enummember       = {link = "_enum"},
+    _typeparam        = {link = "_enum"},
+    _interface        = {link = "_function"}, 
+    _constant         = {link = "_var"}, 
+    _cursorlinenumber = {link = "_comment"}, 
 } end
