b2042eefff18734c310bab8a7d62b6482fad6383
tomsadowski <tsadowski93@gmail.com>
2025-10-12 13:10:15 -0400

    lars is good

---
+++ nvim/colors/lars.lua
@@ -0,0 +1,17 @@
+-- lars
+
+local util    = require("color.util")
+local scheme  = require("color.scheme_simple")
+local palette = vim.deepcopy(require("color.palette"))
+
+palette.darkblue[1]   = "#101820" -- _normal bg
+palette.blue[1]       = "#203040" -- _linenumber
+palette.darkcyan[1]   = "#2080c0" -- _comment, _cursorlinenumber
+palette.cyan[1]       = "#00c0d0" -- _string, _macro
+palette.darkyellow[1] = "#b08040" -- _keyword
+palette.yellow[1]     = "#ffd888" -- _normal fg
+palette.green[1]      = "#70ff20" -- _function
+palette.magenta[1]    = "#b888ff" -- _var
+palette.red[1]        = "#ff6060" -- _enum, _typeparam
+
+util.apply(scheme(palette))
--- nvim/colors/lars_save.lua
+++ nvim/colors/lars_save.lua
@@ -1,4 +1,4 @@
--- larsday
+-- lars
 
 local util    = require("color.util")
 local scheme  = require("color.scheme_simple")
@@ -6,7 +6,7 @@ local palette = vim.deepcopy(require("color.palette"))
 
 palette.darkblue[1]   = "#081020" -- _normal bg
 palette.blue[1]       = "#102040" -- _linenumber
-palette.darkcyan[1]   = "#2080c0" -- _comment
+palette.darkcyan[1]   = "#2080c0" -- _comment, _cursorlinenumber
 palette.cyan[1]       = "#00c0d0" -- _string, _macro
 palette.darkyellow[1] = "#a07028" -- _keyword
 palette.yellow[1]     = "#e0c870" -- _normal fg
