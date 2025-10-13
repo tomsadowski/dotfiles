3320fff1007320b5b899c69d232740f26cb1e2c0
tomsadowski <tsadowski93@gmail.com>
2025-10-13 19:23:58 -0400

    perfection?

--- nvim/colors/llars.lua
+++ nvim/colors/llars.lua
@@ -4,19 +4,19 @@ local util    = require("color.util")
 local scheme  = require("color.scheme_simple")
 local palette = vim.deepcopy(require("color.palette"))
 
-palette.black[1]      = "#000000" -- _normal bg
+palette.black[1]      = "#080808" -- _normal bg
 palette.darkblue[1]   = "#202020" -- _visual, _search
 
 palette.darkgrey[1]   = "#606040" -- _linenumber
 palette.darkcyan[1]   = "#606040" -- _comment
 
-palette.darkyellow[1] = "#707030" -- _keyword
-palette.yellow[1]     = "#c0c040" -- _normal fg
+palette.cyan[1]       = "#00ffff" -- _string, _macro
 
-palette.green[1]      = "#20c060" -- _function
-palette.magenta[1]    = "#c060c0" -- _var
+palette.darkyellow[1] = "#808000" -- _keyword
+palette.yellow[1]     = "#e0e020" -- _normal fg
 
-palette.cyan[1]       = "#2080c0" -- _string, _macro
+palette.green[1]      = "#00ff00" -- _function
+palette.magenta[1]    = "#e060e0" -- _var
 palette.red[1]        = "#ff4040" -- _enum, _typeparam
 
 util.apply(scheme(palette))
