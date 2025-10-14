476830817fb970dd5155113de4a6a75c2d082921
tomsadowski <tsadowski93@gmail.com>
2025-10-14 12:02:05 -0400

    perfection likely

--- nvim/colors/lars.lua
+++ nvim/colors/lars.lua
@@ -7,15 +7,15 @@ local palette = vim.deepcopy(require("color.palette"))
 palette.black[1]       = "#000000" -- _normal bg
 palette.darkblue[1]    = "#101010" -- _visual, _search
 
-palette.darkcyan[1]    = "#2080b0" -- _string, _macro
-palette.darkyellow[1]  = "#a08020" -- _markup
+palette.darkcyan[1]    = "#2080d0" -- _string, _macro
+palette.darkyellow[1]  = "#d06040" -- _markup
 
 palette.darkgreen[2]   = "#406040" -- _comment
 palette.darkgreen[1]   = "#408040" -- _keyword
 palette.green[1]       = "#60d060" -- _normal fg
 
-palette.yellow[1]      = "#c0c000" -- _function
-palette.magenta[1]     = "#c040c0" -- _var
-palette.red[1]         = "#c05050" -- _enum, _typeparam
+palette.yellow[1]      = "#d0b000" -- _function
+palette.magenta[1]     = "#d050b0" -- _var
+palette.red[1]         = "#d06040" -- _enum, _typeparam
 
 util.apply(scheme(palette))
