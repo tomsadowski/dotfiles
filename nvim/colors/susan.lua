25b9f428637ff5a691ae23021051f35ab72a2bbc
tomsadowski <tsadowski93@gmail.com>
2025-10-17 07:24:56 -0400

    this is susan

--- nvim/colors/susan.lua
+++ nvim/colors/susan.lua
@@ -5,19 +5,14 @@ local util    = require("color.util")
 local term    = require("color.term")
 local palette = vim.deepcopy(require("color.palette"))
 
-palette.black[1]       = "#060708" -- _normal bg
-
-palette.darkblue[1]    = "#042020" -- _visual, _search
-
+palette.black[1]       = "#04080c" -- _normal bg
+palette.darkblue[1]    = "#102030" -- _visual, _search
 palette.darkcyan[1]    = "#506070" -- _comment, _linenumber
-palette.darkgreen[1]   = "#30c8b0" -- _keyword
-
-palette.green[1]       = "#b0e830" -- _function
+palette.darkgreen[1]   = "#40c8b0" -- _keyword, _macro
+palette.green[1]       = "#a0e840" -- _function, _interface
 palette.magenta[1]     = "#d860e8" -- _var
-
-palette.cyan[1]        = "#30a0ff" -- _string, _macro
-palette.red[1]         = "#ff7030" -- _enum, _typeparam
-
-palette.white[1]       = "#e8e8e8" -- _normal bg
+palette.cyan[1]        = "#40a0e8" -- _string, _preproc
+palette.red[1]         = "#e87060" -- _enum, _typeparam, _markup
+palette.white[1]       = "#d8d8d8" -- _normal fg
 
 util.apply(scheme(term, palette))
