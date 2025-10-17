32e15d6f9a54937c44372332714beee87c408665
tomsadowski <tsadowski93@gmail.com>
2025-10-17 06:43:26 -0400

    mint is goodgood

--- nvim/colors/mint.lua
+++ nvim/colors/mint.lua
@@ -10,10 +10,10 @@ palette.black[1]       = "#06080a" -- _normal bg
 palette.darkblue[1]    = "#042020" -- _visual, _search
 
 palette.darkcyan[1]    = "#406080" -- _comment, _linenumber
-palette.darkgreen[1]   = "#88c8b0" -- _keyword
+palette.darkgreen[1]   = "#48c8b0" -- _keyword
 
-palette.green[1]       = "#a0e830" -- _function
-palette.magenta[1]     = "#d878ff" -- _var
+palette.green[1]       = "#b0e830" -- _function
+palette.magenta[1]     = "#d878e8" -- _var
 
 palette.cyan[1]        = "#20b0ff" -- _string, _macro
 palette.red[1]         = "#ff7038" -- _enum, _typeparam
