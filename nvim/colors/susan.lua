96056d83241b2651346433c7f1604fd0ed89de97
tomsadowski <tsadowski93@gmail.com>
2025-10-17 22:10:26 -0400

    perfect susanity

--- nvim/colors/susan.lua
+++ nvim/colors/susan.lua
@@ -8,15 +8,11 @@ local palette = vim.deepcopy(require("color.palette"))
 palette.black[1]       = "#04080c" -- _normal bg
 palette.darkblue[1]    = "#102030" -- _visual, _search
 palette.darkcyan[1]    = "#506070" -- _comment, _linenumber
--- palette.darkgreen[1]   = "#40c8b0" -- _keyword, _macro
--- palette.darkgreen[1]   = "#68a888" -- _keyword, _macro
-palette.darkgreen[1]   = "#20c880" -- _keyword, _macro
--- palette.green[1]       = "#a8c868" -- _function, _interface
-palette.green[1]       = "#b8c848" -- _function, _interface
-palette.magenta[1]     = "#c078e8" -- _var
-palette.cyan[1]        = "#50a0e8" -- _string, _preproc
--- palette.red[1]         = "#e89080" -- _enum, _typeparam, _markup
-palette.red[1]         = "#e868a0" -- _enum, _typeparam, _markup
-palette.white[1]       = "#c0c0c0" -- _normal fg
+palette.darkgreen[1]   = "#6cc890" -- _keyword, _macro
+palette.green[1]       = "#c8d858" -- _function, _interface
+palette.magenta[1]     = "#b888ff" -- _var
+palette.cyan[1]        = "#80b0e8" -- _string, _preproc
+palette.red[1]         = "#e888a0" -- _enum, _typeparam, _markup
+palette.white[1]       = "#c8c8c8" -- _normal fg
 
 util.apply(scheme(term, palette))
