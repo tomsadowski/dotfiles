da79c3f5cb9a1e094cca6efe2822d360932f03c2
tomsadowski <tsadowski93@gmail.com>
2025-10-18 06:52:28 -0400

    susans seasons

--- nvim/colors/susan.lua
+++ nvim/colors/susan.lua
@@ -8,11 +8,11 @@ local palette = vim.deepcopy(require("color.palette"))
 palette.black[1]       = "#04080c" -- _normal bg
 palette.darkblue[1]    = "#102030" -- _visual, _search
 palette.darkcyan[1]    = "#506070" -- _comment, _linenumber
-palette.darkgreen[1]   = "#6cc890" -- _keyword, _macro
-palette.green[1]       = "#c8d858" -- _function, _interface
-palette.magenta[1]     = "#b888ff" -- _var
-palette.cyan[1]        = "#80b0e8" -- _string, _preproc
-palette.red[1]         = "#e888a0" -- _enum, _typeparam, _markup
+palette.darkgreen[1]   = "#40c890" -- _keyword, _macro
+palette.green[1]       = "#a8c848" -- _function, _interface
+palette.magenta[1]     = "#c080e8" -- _var
+palette.cyan[1]        = "#68a8e8" -- _string, _preproc
+palette.red[1]         = "#e878a0" -- _enum, _typeparam, _markup
 palette.white[1]       = "#c8c8c8" -- _normal fg
 
 util.apply(scheme(term, palette))
