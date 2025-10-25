b306c3b2baaa09e9bb8288e0dfcd97f61d002ca2
tomsadowski <tsadowski93@gmail.com>
2025-10-25 11:49:18 -0400

    pine is perfect

--- nvim/colors/pine.lua
+++ nvim/colors/pine.lua
@@ -10,27 +10,27 @@ require "treesitter_highlight" {
     changed    = {10, 10, "#89f098"}, -- green
     removed    = {10, 10, "#89f098"}, -- green
 
-    linenumber = {10, 10, "#807060"}, -- brown
-    curlinenum = {10, 10, "#807060"}, -- brown
-    comment    = {10, 10, "#807060"}, -- brown
+    linenumber = {10, 10, "#707070"}, -- brown
+    curlinenum = {10, 10, "#707070"}, -- brown
+    comment    = {10, 10, "#707070"}, -- brown
     visualbg   = {2, 2,   "#343434"}, -- darkbrown
-    visual     = {10, 10, "#e8d098"}, -- orange
+    visual     = {10, 10, "#c8b880"}, -- white
 
-    text       = {10, 10, "#e8d098"}, -- orange
-    identifier = {10, 10, "#e8d098"}, -- orange
+    text       = {10, 10, "#c8b880"}, -- white
+    identifier = {10, 10, "#c8b880"}, -- white
 
-    func       = {9, 9,   "#70b890"}, -- yellowgreen
+    func       = {9, 9,   "#70a8e8"}, -- pine
 
     warnbg     = {10, 10, "#141420"}, -- darkbluemagenta
-    warn       = {10, 10, "#a890e8"}, -- bluemagenta
-    keyword    = {12, 12, "#a890e8"}, -- bluemagenta
-    str        = {12, 12, "#a890e8"}, -- bluemagenta
+    warn       = {10, 10, "#b880d8"}, -- bluemagenta
+    keyword    = {12, 12, "#b880d8"}, -- bluemagenta
+    str        = {12, 12, "#b880d8"}, -- bluemagenta
 
 
     errorbg    = {1, 1,   "#201414"}, -- darkredmagenta
-    error      = {10, 10, "#e878a0"}, -- redmagenta
-    type       = {9, 9,   "#e878a0"}, -- redmagenta
-    enum       = {9, 9,   "#e878a0"}, -- redmagenta
-    class      = {9, 9,   "#e878a0"}, -- redmagenta
-    struct     = {9, 9,   "#e878a0"}, -- redmagenta
+    error      = {10, 10, "#e68878"}, -- redmagenta
+    type       = {9, 9,   "#e68878"}, -- redmagenta
+    enum       = {9, 9,   "#e68878"}, -- redmagenta
+    class      = {9, 9,   "#e68878"}, -- redmagenta
+    struct     = {9, 9,   "#e68878"}, -- redmagenta
 }
