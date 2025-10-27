23a303454f4c8da6606c9baffcc013bc05c7c772
tomsadowski <tsadowski93@gmail.com>
2025-10-27 18:18:14 -0400

    o

--- nvim/colors/bone.lua
+++ nvim/colors/bone.lua
@@ -1,35 +1,35 @@
 -- bone
 
 require "treesitter_highlight" {
-    window     = { 0,  16, "#000000"}, -- black
-    background = { 0, 233, "#101010"}, -- lightblack
-    curlinenbg = { 7, 233, "#101010"}, -- lightblack
+  window     = { 0,  16, "#000000"}, -- black
+  background = { 0, 233, "#101010"}, -- lightblack
+  curlinenbg = { 7, 233, "#101010"}, -- lightblack
 
-    visualbg   = { 2, 236, "#303030"}, -- darkgrey
-    linenumber = {10,  10, "#707070"}, -- grey
-    curlinenum = {10,  10, "#707070"}, -- grey
+  visualbg   = { 2, 236, "#303030"}, -- darkgrey
+  linenumber = {10,  10, "#707070"}, -- grey
+  curlinenum = {10,  10, "#707070"}, -- grey
 
-    comment    = {10,  65, "#5f875f"}, -- green
+  comment    = {10,  65, "#5faf5f"}, -- green
 
-    visual     = {10, 114, "#b0b087"}, -- bone
-    text       = {10, 144, "#b0b087"}, -- bone
-    identifier = {10, 144, "#b0b087"}, -- bone
+  visual     = {10, 114, "#b0b087"}, -- bone
+  text       = {10, 144, "#b0b087"}, -- bone
+  identifier = {10, 144, "#b0b087"}, -- bone
 
-    keyword    = { 9, 110, "#87a8d7"}, -- blue
-    str        = { 9, 110, "#87a8d7"}, -- blue
-    added      = { 9, 110, "#87a8d7"}, -- blue
+  keyword    = { 9, 110, "#5fa8e0"}, -- blue
+  str        = { 9, 110, "#5fa8e0"}, -- blue
+  added      = { 9, 110, "#5fa8e0"}, -- blue
 
-    warnbg     = { 5,   5, "#141420"}, -- darkpurple
-    warn       = {10, 140, "#b088d8"}, -- purple
-    func       = {10, 140, "#b088d8"}, -- purple
-    macro      = {10, 140, "#b088d8"}, -- purple
-    changed    = {10,  10, "#b088d8"}, -- purple
+  warnbg     = { 5,   5, "#141420"}, -- darkpurple
+  warn       = {10, 140, "#af87d7"}, -- purple
+  func       = {10, 140, "#af87d7"}, -- purple
+  macro      = {10, 140, "#af87d7"}, -- purple
+  changed    = {10,  10, "#af87d7"}, -- purple
 
-    errorbg    = { 1, 1,   "#201414"}, -- darkred
-    error      = { 9, 167, "#d87070"}, -- red
-    type       = { 9, 167, "#d87070"}, -- red
-    enum       = { 9, 167, "#d87070"}, -- red
-    class      = { 9, 167, "#d87070"}, -- red
-    struct     = { 9, 167, "#d87070"}, -- red
-    removed    = { 9, 167, "#d87070"}, -- red
+  errorbg    = { 1, 1,   "#201414"}, -- darkred
+  error      = { 9, 167, "#d75f5f"}, -- red
+  type       = { 9, 167, "#d75f5f"}, -- red
+  enum       = { 9, 167, "#d75f5f"}, -- red
+  class      = { 9, 167, "#d75f5f"}, -- red
+  struct     = { 9, 167, "#d75f5f"}, -- red
+  removed    = { 9, 167, "#d75f5f"}, -- red
 }
--- nvim/init.lua
+++ nvim/init.lua
@@ -21,8 +21,8 @@ vim.o.scrolloff = 10
 vim.o.undofile = false
 vim.o.wrap = true
 vim.o.expandtab = true
-vim.o.shiftwidth=4
-vim.o.tabstop=4
+vim.o.shiftwidth=2
+vim.o.tabstop=2
 vim.o.number = true   
 vim.o.laststatus = 1  
 vim.o.foldmethod = 'syntax'
@@ -122,76 +122,74 @@ vim.keymap.set({"n"}, "<C-Up>", "<C-o>")          -- previous jump
 
 -- LSP ----------------------------------------------------------
 
-    vim.api.nvim_create_autocmd(
-        {'BufEnter', 'BufWinEnter'}, {
-            pattern = {
-                '*.c', '*.C', '*.h', '*.H', 
-                '*.cpp', '*.CPP', '*.hpp', '*.HPP'
-            },
-            callback = function(args) 
-                vim.lsp.start({
-                        name = 'clang-lsp',
-                        cmd = {'clangd'},
-                        root_dir = vim.fs.root(
-                            args.buf, 
-                            {'.git'}
-                        )
-                    }
-                )
-            end
+vim.api.nvim_create_autocmd(
+  {'BufEnter', 'BufWinEnter'}, 
+  {
+    pattern = {
+      '*.c', '*.C', '*.h', '*.H', 
+      '*.cpp', '*.CPP', '*.hpp', '*.HPP'
+    },
+    callback = function(args) 
+      vim.lsp.start {
+        name = 'clang-lsp',
+        cmd = {'clangd'},
+        root_dir = vim.fs.root(
+          args.buf, 
+          {'.git'}
+        )
+      }
+    end
+  }
+)
+vim.api.nvim_create_autocmd(
+  'FileType', 
+  {
+    pattern  = 'rust',
+    callback = function(args) 
+      vim.lsp.start {
+        name = 'rust-lsp',
+        cmd = {'rust-analyzer'},
+        root_dir = vim.fs.root(
+            args.buf, 
+            {'Cargo.toml'}
+          )
         }
-    )
-    vim.api.nvim_create_autocmd(
-        'FileType', {
-            pattern  = 'rust',
-            callback = function(args) 
-                vim.lsp.start({
-                        name = 'rust-lsp',
-                        cmd = {'rust-analyzer'},
-                        root_dir = vim.fs.root(
-                            args.buf, 
-                            {'Cargo.toml'}
-                        )
-                    }
-                )
-            end
-        }
-    )
-    vim.api.nvim_create_autocmd(
-        {'BufEnter', 'BufWinEnter'}, {
-            pattern  = '*.cs',
-            callback = function(args) 
-                vim.lsp.start({
-                        name = 'csharp-lsp',
-                        cmd = {'csharp-ls'},
-                        root_dir = vim.fs.root(
-                            args.buf, 
-                            {'.git'}
-                        )
-                    }
-                )
-            end
-        }
-    )
-    vim.diagnostic.config {
-        underline = true,
-        float = true,
-    --    virtual_text = true,
-        update_in_insert = false,
-    }
-    vim.lsp.config('*', {
-        root_markers = { '.git' },
-    })
-    vim.lsp.config.clangd = {
-        cmd = {
-            'clangd',
-            '--clang-tidy',
-            '--background-index',
-            '--offset-encoding=utf-8',
-        },
-        root_markers = { '.clangd', 'compile_commands.json' },
-        filetypes = { 'c', 'cpp' },
-    }
+      end
+  }
+)
+vim.api.nvim_create_autocmd(
+  {'BufEnter', 'BufWinEnter'}, {
+    pattern  = '*.cs',
+    callback = function(args) 
+      vim.lsp.start {
+        name = 'csharp-lsp',
+        cmd = {'csharp-ls'},
+        root_dir = vim.fs.root(
+          args.buf, 
+          {'.git'}
+        )
+      }
+    end
+  }
+)
+vim.diagnostic.config {
+  underline = true,
+  float = true,
+  update_in_insert = false,
+}
+vim.lsp.config('*', {
+  root_markers = { '.git' },
+})
+vim.lsp.config.clangd = {
+  cmd = {
+    'clangd',
+    '--clang-tidy',
+    '--background-index',
+    '--offset-encoding=utf-8',
+  },
+  root_markers = { '.clangd', 'compile_commands.json' },
+  filetypes = { 'c', 'cpp' },
+}
 
 
 -- COMMANDS ---------------------------------------------------
