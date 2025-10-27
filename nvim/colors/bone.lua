cd74f116ba6be565a20a73aae480d08aa9d91702
tomsadowski <tsadowski93@gmail.com>
2025-10-26 20:41:30 -0400

    yep

--- nvim/colors/bone.lua
+++ nvim/colors/bone.lua
@@ -1,36 +1,35 @@
 -- bone
 
 require "treesitter_highlight" {
-    window     = {4, 4, "#000000"}, -- black
-    background = {0, 0, "#101010"}, -- grey
-    curlinenbg = {2, 2, "#101010"}, -- darkgrey
+    window     = { 0,  16, "#000000"}, -- black
+    background = { 0, 233, "#101010"}, -- lightblack
+    curlinenbg = { 7, 233, "#101010"}, -- lightblack
 
-    macro      = {10, 10, "#89f098"}, -- green
-    added      = {10, 10, "#89f098"}, -- green
-    changed    = {10, 10, "#89f098"}, -- green
-    removed    = {10, 10, "#89f098"}, -- green
+    visualbg   = { 2, 236, "#303030"}, -- darkgrey
+    linenumber = {10,  10, "#707070"}, -- grey
+    curlinenum = {10,  10, "#707070"}, -- grey
 
-    linenumber = {10, 10, "#707070"}, -- brown
-    curlinenum = {10, 10, "#707070"}, -- brown
-    comment    = {10, 10, "#708870"}, -- green
-    visualbg   = {2, 2,   "#343434"}, -- darkbrown
-    visual     = {10, 10, "#c0b880"}, -- white
+    comment    = {10,  65, "#5f875f"}, -- green
 
-    text       = {10, 10, "#c8b898"}, -- bone
-    identifier = {10, 10, "#c8b898"}, -- bone
+    visual     = {10, 114, "#b0b087"}, -- bone
+    text       = {10, 144, "#b0b087"}, -- bone
+    identifier = {10, 144, "#b0b087"}, -- bone
 
-    warnbg     = {10, 10, "#141420"}, -- darkpurple
-    warn       = {10, 10, "#b088d8"}, -- purple
-    func       = {10, 10, "#b088d8"}, -- purple
+    keyword    = { 9, 110, "#87a8d7"}, -- blue
+    str        = { 9, 110, "#87a8d7"}, -- blue
+    added      = { 9, 110, "#87a8d7"}, -- blue
 
-    keyword    = {9, 9,   "#70a8e0"}, -- blue
-    str        = {9, 9,   "#70a8e0"}, -- blue
+    warnbg     = { 5,   5, "#141420"}, -- darkpurple
+    warn       = {10, 140, "#b088d8"}, -- purple
+    func       = {10, 140, "#b088d8"}, -- purple
+    macro      = {10, 140, "#b088d8"}, -- purple
+    changed    = {10,  10, "#b088d8"}, -- purple
 
-
-    errorbg    = {1, 1,   "#201414"}, -- darkred
-    error      = {10, 10, "#e07268"}, -- red
-    type       = {9, 9,   "#e07268"}, -- red
-    enum       = {9, 9,   "#e07268"}, -- red
-    class      = {9, 9,   "#e07268"}, -- red
-    struct     = {9, 9,   "#e07268"}, -- red
+    errorbg    = { 1, 1,   "#201414"}, -- darkred
+    error      = { 9, 167, "#d87070"}, -- red
+    type       = { 9, 167, "#d87070"}, -- red
+    enum       = { 9, 167, "#d87070"}, -- red
+    class      = { 9, 167, "#d87070"}, -- red
+    struct     = { 9, 167, "#d87070"}, -- red
+    removed    = { 9, 167, "#d87070"}, -- red
 }
