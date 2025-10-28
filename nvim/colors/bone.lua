3fd4a3ae5b59bd05fb40b3eed98f08d3353a017c
tomsadowski <tsadowski93@gmail.com>
2025-10-28 08:20:23 -0400

    better

--- nvim/colors/bone.lua
+++ nvim/colors/bone.lua
@@ -1,5 +1,4 @@
 -- bone
-
 require "treesitter_highlight" {
   window     = { 0,  16, "#000000"}, -- black
   background = { 0, 233, "#101010"}, -- lightblack
@@ -9,27 +8,27 @@ require "treesitter_highlight" {
   linenumber = {10,  10, "#707070"}, -- grey
   curlinenum = {10,  10, "#707070"}, -- grey
 
-  comment    = {10,  65, "#5faf5f"}, -- green
+  comment    = {10,  65, "#607060"}, -- green
 
-  visual     = {10, 114, "#b0b087"}, -- bone
-  text       = {10, 144, "#b0b087"}, -- bone
-  identifier = {10, 144, "#b0b087"}, -- bone
+  visual     = {10, 114, "#a8a888"}, -- bone
+  text       = {10, 144, "#a8a888"}, -- bone
+  identifier = {10, 144, "#a8a888"}, -- bone
 
-  keyword    = { 9, 110, "#5fa8e0"}, -- blue
-  str        = { 9, 110, "#5fa8e0"}, -- blue
-  added      = { 9, 110, "#5fa8e0"}, -- blue
+  keyword    = { 9, 110, "#60a0c0"}, -- blue
+  str        = { 9, 110, "#60a0c0"}, -- blue
+  added      = { 9, 110, "#60a0c0"}, -- blue
 
   warnbg     = { 5,   5, "#141420"}, -- darkpurple
-  warn       = {10, 140, "#af87d7"}, -- purple
-  func       = {10, 140, "#af87d7"}, -- purple
-  macro      = {10, 140, "#af87d7"}, -- purple
-  changed    = {10,  10, "#af87d7"}, -- purple
+  warn       = {10, 140, "#a090b0"}, -- purple
+  func       = {10, 140, "#a090b0"}, -- purple
+  macro      = {10, 140, "#a090b0"}, -- purple
+  changed    = {10,  10, "#a090b0"}, -- purple
 
   errorbg    = { 1, 1,   "#201414"}, -- darkred
-  error      = { 9, 167, "#d75f5f"}, -- red
-  type       = { 9, 167, "#d75f5f"}, -- red
-  enum       = { 9, 167, "#d75f5f"}, -- red
-  class      = { 9, 167, "#d75f5f"}, -- red
-  struct     = { 9, 167, "#d75f5f"}, -- red
-  removed    = { 9, 167, "#d75f5f"}, -- red
+  error      = { 9, 167, "#c06860"}, -- red
+  type       = { 9, 167, "#c06860"}, -- red
+  enum       = { 9, 167, "#c06860"}, -- red
+  class      = { 9, 167, "#c06860"}, -- red
+  struct     = { 9, 167, "#c06860"}, -- red
+  removed    = { 9, 167, "#c06860"}, -- red
 }
